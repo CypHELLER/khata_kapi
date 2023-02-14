87042c2467fd05c0325ce1ac6ec3efb279c85543
SthBishal <np03cs4s210268@heraldcollege.edu.np>
2023-02-14 13:11:34 +0545

    New font added

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:khatakapi/otp_Eng.dart';
 
@@ -14,56 +13,116 @@ class _RegistrationState extends State<Registration> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Image.asset(
-          'assets/images/logo.png',
-          width: 100,
-        ),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.chevron_left_rounded,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const OtpEng(),
-              ),
-            );
-          },
-        ),
-        actions: <Widget>[
-          Padding(
-            padding:
-            const EdgeInsets.only(right: 20.0),
-            child: GestureDetector(
-              onTap: () {
-                exit(0);
-               },
-              child: const Icon(
-                Icons.cancel,
-                size: 26.0,
-                color: Colors.red,
-               ),
-             ),
-           ),
-         ],
-        
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background_image2.JPG'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Stack(
-          fit: StackFit.expand,
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            Align(
-              alignment: Alignment.topLeft,
+            Container(
+              height: 250,
+              decoration: const BoxDecoration(
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(90)),
+                color: Colors.amber,
+                gradient: LinearGradient(
+                  colors: [
+                    (Color.fromARGB(255, 148, 121, 163)),
+                    Color.fromARGB(255, 24, 210, 213)
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: GestureDetector(
+                      onTap: () {
+                        exit(0);
+                      },
+                      child: const Icon(
+                        Icons.cancel,
+                        size: 35.0,
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 50),
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      height: 100,
+                      width: 120,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 20, top: 20),
+                    alignment: Alignment.bottomRight,
+                    child: const Text(
+                      "Enter Your Details",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ],
+              )),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 70),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.grey[200],
+                boxShadow: const [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Color(0xffEEEEEE)),
+                ],
+              ),
+              child: const TextField(
+                cursorColor: Colors.green,
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.green,
+                  ),
+                  hintText: "Full Name",
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.grey[200],
+                boxShadow: const [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Color(0xffEEEEEE)),
+                ],
+              ),
+              child: const TextField(
+                cursorColor: Colors.green,
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.email,
+                    color: Colors.green,
+                  ),
+                  hintText: "Email",
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
             ),
           ],
         ),
